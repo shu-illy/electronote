6dbc12aaf6649ae80851c987d2be4491c03a07cd
shu-illy <sh.illy.9117@gmail.com>
2021-01-17 17:09:34 +0900

    Change repository name

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | EleCLIP"
+  end
+
+end
