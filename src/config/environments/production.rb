75f0e4873ad4061a72b50db536cf886fa63fe151
shu-illy <sh.illy.9117@gmail.com>
2021-01-25 23:20:10 +0900

    Modified App name

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "eleclip_production"
+  # config.active_job.queue_name_prefix = "electronote_production"
 
   config.action_mailer.perform_caching = false
 
