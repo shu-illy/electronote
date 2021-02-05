8fd4079703b8a467f727d91a3c067785897dcba2
shu-illy <sh.illy.9117@gmail.com>
2021-02-05 10:16:54 +0900

    Link master.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,7 @@ set :deploy_to, '/var/www/electronote'
 
 # シンボリックリンクをはるファイル。(※後述)
 set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
 # シンボリックリンクをはるフォルダ。(※後述)
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
