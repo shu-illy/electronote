efa2459756aa02a7e93ddf8fa02722290ad55b1b
shu-illy <sh.illy.9117@gmail.com>
2021-02-05 10:29:02 +0900

    Link master.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,9 +7,6 @@ set :repo_url, "git@github.com:shu-illy/electronote.git"
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
