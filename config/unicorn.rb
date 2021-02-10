07a31f297d8c50ba6c69de90ddc93b36de5c1c63
shu-illy <sh.illy.9117@gmail.com>
2021-02-11 02:27:11 +0900

    Unicorn, Nginx,MySQL on Docker構築完了

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,15 +2,16 @@ $worker = 2
 $timeout = 30
 # $app_dir = File.expand_path "../", __FILE__
 $app_dir = '/electronote'
-$listen = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+$listen = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
 $pid = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-$std_log = File.expand_path 'log/unicorn.log', $app_dir
+$stdout_log = File.expand_path 'log/unicorn.log', $app_dir
+$stderr_log = File.expand_path 'log/unicorn_error.log', $app_dir
 
 # 上記で設定したものが適応されるよう定義
 worker_processes $worker
 working_directory $app_dir
-stderr_path $std_log
-stdout_path $std_log
+stdout_path $stdout_log
+stderr_path $stderr_log
 timeout $timeout
 listen $listen
 pid $pid
