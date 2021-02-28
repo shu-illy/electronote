45244e85c84ff57dbc6aace5bc72b4cb538dd7d8
shu-illy <sh.illy.9117@gmail.com>
2021-02-28 11:23:53 +0900

    Add site_layout feature spec

---
+++ spec/features/site_layouts_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "SiteLayoutTest", type: :feature do
+
+  scenario "layout links" do
+    visit root_path
+    expect(page).to have_link nil, href: root_path, count: 2
+    expect(page).to have_link nil, href: help_path
+    expect(page).to have_link nil, href: contact_path
+    visit contact_path
+    expect(page).to have_title full_title("Contact")
+    visit signup_path
+    expect(page).to have_title full_title("Sign up")
+  end
+      
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,8 +36,10 @@ end
 # ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 RSpec.configure do |config|
+  include ApplicationHelper
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
